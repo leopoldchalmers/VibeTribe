5c1e6659d853e7fb676c22d2631149ae330b7724
leopoldchalmers <leopoldw@chalmers.se>
2025-02-11 14:58:45 +0100

    Installed react and added userService
    Co-Authored-By: EmilAndersso <113036374+EmilAndersso@users.noreply.github.com>
    Co-Authored-By: hannamagn <117651421+hannamagn@users.noreply.github.com>
    Co-Authored-By: hildalange <117651449+hildalange@users.noreply.github.com>
    Co-Authored-By: senja <113063952+senjakortesaari@users.noreply.github.com>

--- server/src/start.ts
+++ server/src/start.ts
@@ -1,9 +1,13 @@
 import express from "express";
 import { tribeRouter } from "./router/tribeRouter";
 import { postRouter } from "./router/postRouter";
+import { userRouter } from "./router/userRouter";
+import cors from "cors";
 
 export const app = express();
 
 app.use(express.json());
+app.use(cors());
 app.use("/tribes", tribeRouter);
 app.use("/posts", postRouter);
+app.use("/users", userRouter);
