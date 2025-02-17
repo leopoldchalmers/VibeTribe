360e74e6a74e5d8de5da66b107967be0afadc84b
hannamagn <117651421+hannamagn@users.noreply.github.com>
2025-02-17 09:45:06 +0100

    Nu kan man logga ut också

--- client/src/components/Account.tsx
+++ client/src/components/Account.tsx
@@ -20,38 +20,44 @@ function Account() {
 
     return (
         <>
-        <div className="d-grid gap-2 col-6 mx-auto">
-          {isLoggedIn ? (
-            <h1 className="pageTitle">Welcome back, {user?.name}!</h1> 
+          <div className="d-grid gap-2 col-6 mx-auto">
+            {isLoggedIn ? (
+              <h1 className="pageTitle">Welcome back, {user?.name}!</h1>
             ) : (
-            <h1 className="pageTitle">Log in to VibeTribe </h1>
-          )}
-        </div>   
-      
-    
-    <Form className="form-container">
-      {isLoggedIn ? null : (
-        <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-        <Form.Label>Email address</Form.Label>
-        <Form.Control type="email" placeholder="name@example.com" />
-        <Form.Label>Password</Form.Label>
-        <Form.Control type="password" placeholder="Password" />
-      </Form.Group>
-      )}
-    </Form>
-            {isLoggedIn ? null : (
-            <div className="d-grid gap-2 col-6 mx-auto">
-
-            <button type="button" className="btn btn-light loginButton" >Log in</button>
-            <p className="signUptext">Don't have an account?</p>  
-            <button type="button" className="btn btn-light signUpButton" onClick={()=> navigate("/signup")}  >Sign up</button>
-            </div>
+              <h1 className="pageTitle">Log in to VibeTribe</h1>
             )}
-        </>
-      
-    )
- 
-} 
+          </div>
 
+          <Form className="form-container">
+            {isLoggedIn ? null : (
+              <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+                <Form.Label>Email address</Form.Label>
+                <Form.Control type="email" placeholder="name@example.com" />
+                <Form.Label>Password</Form.Label>
+                <Form.Control type="password" placeholder="Password" />
+              </Form.Group>
+            )}
+          </Form>
+
+          {isLoggedIn ? null : (
+            <div className="d-grid gap-2 col-6 mx-auto">
+              <button type="button" className="btn btn-light loginButton">Log in</button>
+              <p className="signUptext">Don't have an account?</p>
+              <button type="button" className="btn btn-light signUpButton" onClick={() => navigate("/signup")}>Sign up</button>
+            </div>
+          )}
+
+          {isLoggedIn && (
+            <button className="btn btn-light logoutButton" onClick={() => {
+              localStorage.removeItem("user");
+              setIsLoggedIn(false);
+              setUser(null);
+            }}>
+              Logout
+            </button>
+          )}
+        </>
+    );
+}
 
 export default Account;
