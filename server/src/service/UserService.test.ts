36a54fac641e51da3a45b938137ad770bc9796c5
leopold-wahlbeck <leopoldwahlbeck@gmail.com>
2025-03-13 19:39:02 +0100

    added tests for creating user
    Co-Authored-By: Leopold Wahlbeck <112471595+leopoldchalmers@users.noreply.github.com>
    Co-Authored-By: EmilAndersso <113036374+EmilAndersso@users.noreply.github.com>

--- server/src/service/UserService.test.ts
+++ server/src/service/UserService.test.ts
@@ -27,10 +27,10 @@ beforeAll(async () => {
     userService = new UserService();
 })
 
+    //sign up test start
+describe("Creating User Tests", () => {
 
-describe("UserService Tests", () => {
-
-    //Login test start
+   
 
     test("User authentication should pass with valid credentials", async () => {
         await userService.createUser(user.username, user.email, user.password);
@@ -40,38 +40,115 @@ describe("UserService Tests", () => {
     }
     );
 
-    test("User authentication should fail with invalid password", async () => {
-        
+    test("Return error if username is empty", async () => {
+        await userService.createUser("", user.email, user.password);
+        const foundUser = await userService.findUser("", user.password);
+        expect(foundUser).toBeUndefined();
+    }
+    );
 
+    test("Return error if email is empty", async () => {
+        await userService.createUser(user.username, "", user.password);
+        const foundUser = await userService.findUser(user.username, user.password);
+        expect(foundUser).toBeUndefined();
+    }
+    );
+
+    test("Return error if password is empty", async () => {
+        await userService.createUser(user.username, user.email, "");
+        const foundUser = await userService.findUser(user.username, "");
+        expect(foundUser).toBeUndefined();
+    }
+    );
+
+    test("User cant be created with same username", async () => {
+        await userService.createUser(user.username, user.email, user.password);
+        const foundUser = await userService.createUser(user.username, user.email, user.password);
+        expect(foundUser).toBeUndefined();
+        await userService.removeUser(user.username);
+    }
+    );
+
+    test("User cant be created with same email", async () => {
+        await userService.createUser(user.username, user.email, user.password);
+        const foundUser = await userService.createUser("newuser", user.email, user.password);
+        expect(foundUser).toBeUndefined();
+        await userService.removeUser(user.username);
+    }
+    );
+
+    test("Check password encryption when creating user", async () => {
+        await userService.createUser(user.username, user.email, user.password);
+        const foundUser = await UserModel.findOne({ where: { username: user.username } });
+        expect(foundUser?.password).not.toBe(user.password);
+        await userService.removeUser(user.username);
+    }
+    );
+
+    test("User must have valid email format (email must contain @ and .something)", async () => {
+        const invalidEmail1 = "testgmail.com";
+        const invalidEmail2 = "test@gmailcom";
+
+        //Creating user with valid email
+        await userService.createUser(user.username, user.email, user.password);
+        const foundUser = await userService.findUser(user.username, user.password);
+        expect(foundUser).toBeDefined();
+        await userService.removeUser(user.username);
+
+        //Creating user without @
+        await userService.createUser(user.username, invalidEmail1, user.password);
+        const foundUser2 = await userService.findUser(user.username, user.password);
+        expect(foundUser2).toBeUndefined();
+        await userService.removeUser(user.username);
+
+        //Creating user without .
+        await userService.createUser(user.username, invalidEmail2, user.password);
+        const foundUser3 = await userService.findUser(user.username, user.password);
+        expect(foundUser3).toBeUndefined();
+        await userService.removeUser(user.username);
+    }
+    );
+
+
+
+});
+
+    
+    //sign up test end
+
+
+
+
+     //Login test start
+
+describe("Login User Tests", () => {
+    
+    test("User authentication should fail with invalid password", async () => {
         await userService.createUser(user.username, user.email, user.password);
         const foundUser = await userService.findUser(user.username,"wrongpassword");
         expect(foundUser).toBeUndefined();
         await userService.removeUser(user.username);
     });
-});
 
-    
+    test("User authentication should fail with invalid username", async () => {
+        await userService.createUser(user.username, user.email, user.password);
+        const foundUser = await userService.findUser("wrongusername", user.password);
+        expect(foundUser).toBeUndefined();
+        await userService.removeUser(user.username);
+    });
+
+}
+);
+
+
+
 
     // login test end
 
 
 
-    //sign up test start
-
-  /*      
-test("User registration should fail with invalid email", async () => {
-    const username = "user";
-    const email = "user@gmail.com";
-    const password = "password";
-    const wrongEmail = "wrongemail";
-
-    const userService = new UserService();
-
-    await userService.createUser(username, email, password);
-    const foundUser = await userService.findUser(username, password);
-    expect(foundUser).toBeUndefined();
-});
     
-*/
 
-    //sign up test end
+ 
+    
+
--- server/src/service/UserService.ts
+++ server/src/service/UserService.ts
@@ -6,16 +6,32 @@ import { UserModel } from '../db/user.db';
 
 export class UserService {
 
-  async createUser(name: string, email: string, password: string): Promise<UserModel> {
-    console.log("encrypt password");
+  async createUser(name: string, email: string, password: string): Promise<UserModel | undefined> {
 
-   // if (await UserModel.findOne({where: {username: name}})) {
-    //  return null;
- // }
+
+
+    // Instead of returning undefined, we should throw an error?
+    // Instead of if-statements we could use case-switch?
+    if (!name || !email || !password) {
+      return undefined;
+    }
+
+   if (await UserModel.findOne({where: {username: name}})) {
+     return undefined;
+    }
+
+    // Email validation
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailRegex.test(email)) {
+      return undefined;
+    }
+    
+
+  if (await UserModel.findOne({where: {email: email}})) {
+    return undefined;
+  }
+
     const salt = bcrypt.genSaltSync(10);
-    console.log("username: "+ name);
-    console.log("email: "+ email);
-    console.log("password: "+ password);
     return await UserModel.create({
       username: name,
       email: email,
