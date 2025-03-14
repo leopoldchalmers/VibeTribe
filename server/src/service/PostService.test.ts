a878af55da30f9b64c72dbbf327d06013e668917
leopold-wahlbeck <leopoldwahlbeck@gmail.com>
2025-03-14 15:44:23 +0100

    Fixed tests after latest pushes
    Fixed tests in UserService, PostService, and start.
    
    To Do:
    * Maybe so taht TribeService takes UserService as a argument?
    * Fix tests in TribeService

--- server/src/service/PostService.test.ts
+++ server/src/service/PostService.test.ts
@@ -4,26 +4,26 @@ import { Tribe } from "../model/tribe";
 
 describe("PostService Tests", () => {
 
-test("If a post is created, it should be returned to list", async () => {
-    const title = "Post title";
-    const description = "Post description";
-    const author: User = {
-        username: "Author name",
-        email: "author@gmail.com",
-        password: "authorpassword"
-    }
-    const tribe: Tribe = {
-        title: "Tribe title",
-        id: 0,
-        description: "Tribe description",
-        posts: [],
-        createdAt: new Date(Date.now()).toLocaleDateString(),
-        updatedAt: new Date(Date.now()).toLocaleDateString()
-    };
-    const postService = new PostService();
-    
-    await postService.addPost(title, description, author, tribe);
-    const posts = await postService.getPosts();
-    expect(posts.some(post => post.title === title)).toBeTruthy();
-});
-});
+    test("If a post is created, it should be returned to list", async () => {
+        const title = "Post title";
+        const description = "Post description";
+        const author: User = {
+            username: "Author name",
+            email: "author@gmail.com",
+            password: "authorpassword"
+        }
+        const tribe: Tribe = {
+            title: "Tribe title",
+            id: 0,
+            description: "Tribe description",
+            owner: "testuser",
+            createdAt: new Date(Date.now()),
+            updatedAt: new Date(Date.now())
+        };
+        const postService = new PostService();
+        
+        await postService.addPost(title, description, author.username, tribe);
+        const posts = await postService.getPosts();
+        expect(posts.some(post => post.title === title)).toBeTruthy();
+    });
+    });
