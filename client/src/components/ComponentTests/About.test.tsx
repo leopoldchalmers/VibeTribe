2b9cc2fad90136e49aaf49fbd4d9838754adb650
EmilAndersso <cem.andersson@gmail.com>
2025-03-18 15:22:12 +0100

    Created frontend test for TribeInfo

--- client/src/components/ComponentTests/About.test.tsx
+++ client/src/components/ComponentTests/About.test.tsx
@@ -1,5 +1,5 @@
 import { render, screen } from '@testing-library/react';
-import { About } from '../About';
+import { About } from '../about';
 import { MemoryRouter } from 'react-router-dom';
 
 describe('About Component', () => {
