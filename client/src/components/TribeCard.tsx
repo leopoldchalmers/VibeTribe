728a832b7c3d0962e82e5fa9dfffd7511e24d233
leopold-wahlbeck <leopoldwahlbeck@gmail.com>
2025-03-20 20:09:46 +0100

    Nicer tribe card

--- client/src/components/TribeCard.tsx
+++ client/src/components/TribeCard.tsx
@@ -16,7 +16,7 @@ export function TribeCard({ tribe }: { tribe: Tribe }) {
       <div key={tribe.id} className="cardComponent">
         <h2 style={{ fontWeight: "bold" }}>{tribe.title}</h2>
         <p>{tribe.description}</p>
-        <p>👥 {tribe.owner} Owner </p>
+        <p>👥 Owner: {tribe.owner} </p>
         <p>📅 {new Date(tribe.createdAt).toLocaleDateString()}</p>
       </div>
     </Link>
